7b0633721a7fb819fc114a1d41f3b8b1c50110ed
Minhazur Rahman Rafi <rafi.rahman03@yahoo,com>
2021-02-07 20:41:37 +0600

    failed to done

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-document.getElementById('submitButton').addEventListener('click', function () {
-    const foodName = document.getElementById('searchFood').value;
+document.getElementById("search-btn").addEventListener("click", getMealList);
+const mealList = document.getElementById("meal");
 
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + foodName)
-        .then(res => res.json())
-        .then(data => searchFood(data));
-
-    const searchFood = foods => {
-        const mealName = foods.meals[0].strMeal;
-        const mealThumb = foods.meals[0].strMealThumb;
-        const foodDiv = document.getElementById('foods');
-        foodDiv.className = 'foodItem'
-        foodDiv.innerHTML = `
-            <img src="${mealThumb}">
-            <h3>${mealName}</h3>
-        `;
-    }
-
-
-
-})
-
-function details() {
-    console.log("details has been shown");
+// get meal list
+function getMealList() {
+  const gettingFoodName = document.getElementById("search-input").value;
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${gettingFoodName}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      let foodList = "";
+      if (data.meals) {
+        data.meals.forEach((meal) => {
+          foodList += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                        </div>
+                    </div>
+                `;
+        });
+        mealList.classList.remove("notFound");
+      } else {
+        foodList = "Sorry, we didn't find your food";
+        mealList.classList.add("notFound");
+      }
+      mealList.innerHTML = foodList;
+    });
 }
