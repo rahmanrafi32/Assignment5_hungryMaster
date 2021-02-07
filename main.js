c5b4f125782751f35f3504b19ca0cd0eafa6ef2e
Rafi Rahman <rafi.rahman03@yahoo.com>
2021-02-07 14:09:55 +0600

    modified function

--- main.js
+++ main.js
@@ -3,15 +3,18 @@ document.getElementById('submitButton').addEventListener('click', function () {
 
     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + foodName)
         .then(res => res.json())
-        .then(data => {
-            const mealName = data.meals[0].strMeal;
-            const mealThumb = data.meals[0].strMealThumb;
-            const foodDiv = document.getElementById('foods');
-            foodDiv.innerHTML = `
-                    <img src="${mealThumb}">
-                    <h3>${mealName}</h3>
-                `
-        });
+        .then(data => searchFood(data));
+
+    const searchFood = foods => {
+        const mealName = foods.meals[0].strMeal;
+        const mealThumb = foods.meals[0].strMealThumb;
+        const foodDiv = document.getElementById('foods');
+        foodDiv.className = 'foodItem'
+        foodDiv.innerHTML = `
+            <img src="${mealThumb}">
+            <h3>${mealName}</h3>
+        `;
+    }
 
 
 
